65f4099b48c10faa6c88ccfdd5250b5b00c3fac4
Rafael Ferreira da Silva <rafael.silva@creatis.insa-lyon.fr>
2011-07-12 14:03:30 +0000

    Feature #521
    git-svn-id: svn+ssh://svn.creatis.insa-lyon.fr/svn/vip/Portal/trunk@1298 97043df2-9288-40f0-b43d-ea39296b91c7

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/client/bean/SimulationInput.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/client/bean/SimulationInput.java
@@ -40,16 +40,16 @@ import com.google.gwt.user.client.rpc.IsSerializable;
  *
  * @author Rafael Silva
  */
-public class WorkflowInput implements IsSerializable {
+public class SimulationInput implements IsSerializable {
 
     private String application;
     private String name;
     private String inputs;
 
-    public WorkflowInput() {
+    public SimulationInput() {
     }
 
-    public WorkflowInput(String application, String name, String inputs) {
+    public SimulationInput(String application, String name, String inputs) {
         this.application = application;
         this.name = name;
         this.inputs = inputs;
