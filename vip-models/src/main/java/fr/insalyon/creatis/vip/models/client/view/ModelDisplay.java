e1a7ed289958ca481e7b0466a0fe1e58dcc350f3
Frederic Cervenansky <frederic.cervenansky@creatis.insa-lyon.fr>
2012-08-10 16:15:46 +0000

    git-svn-id: svn+ssh://svn.creatis.insa-lyon.fr/svn/vip/Portal/trunk@3118 97043df2-9288-40f0-b43d-ea39296b91c7

--- vip-models/src/main/java/fr/insalyon/creatis/vip/models/client/view/ModelDisplay.java
+++ vip-models/src/main/java/fr/insalyon/creatis/vip/models/client/view/ModelDisplay.java
@@ -82,8 +82,8 @@ public class ModelDisplay extends VLayout {
     ModelDisplay(String uri) {
         super();
         buildModel(uri);
-        enableAdd();
-        disableAdd();
+      //  enableAdd();
+        //disableAdd();
     }
 
     ModelDisplay(SimulationObjectModel result, boolean bfull) {
