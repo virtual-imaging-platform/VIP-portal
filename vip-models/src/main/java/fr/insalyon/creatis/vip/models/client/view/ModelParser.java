c4cb6ff7ecd56a9314ee18319f8a15e08da50c2e
Rafael Ferreira da Silva <rafael.silva@creatis.insa-lyon.fr>
2013-02-21 14:19:32 +0000

    Feature #1922
    git-svn-id: svn+ssh://svn.creatis.insa-lyon.fr/svn/vip/Portal/trunk@3765 97043df2-9288-40f0-b43d-ea39296b91c7

--- vip-models/src/main/java/fr/insalyon/creatis/vip/models/client/view/ModelParser.java
+++ vip-models/src/main/java/fr/insalyon/creatis/vip/models/client/view/ModelParser.java
@@ -4,8 +4,6 @@
  * rafael.silva@creatis.insa-lyon.fr
  * http://www.rafaelsilva.com
  *
- * This software is a grid-enabled data-driven workflow manager and editor.
- *
  * This software is governed by the CeCILL  license under French law and
  * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL
