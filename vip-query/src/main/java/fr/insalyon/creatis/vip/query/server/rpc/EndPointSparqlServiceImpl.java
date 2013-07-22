870597f4162d8df591e50060312e2e3ea899cdfe
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2013-07-22 08:23:43 +0000

    22 juillet 2013
    git-svn-id: svn+ssh://svn.creatis.insa-lyon.fr/svn/vip/Portal/trunk@3996 97043df2-9288-40f0-b43d-ea39296b91c7

--- vip-query/src/main/java/fr/insalyon/creatis/vip/query/server/rpc/EndPointSparqlServiceImpl.java
+++ vip-query/src/main/java/fr/insalyon/creatis/vip/query/server/rpc/EndPointSparqlServiceImpl.java
@@ -4,17 +4,16 @@
  */
 package fr.insalyon.creatis.vip.query.server.rpc;
 
-import com.google.gwt.http.client.UrlBuilder;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import fr.insalyon.creatis.vip.query.client.rpc.EndPointSparqlService;
 import java.io.UnsupportedEncodingException;
 import org.apache.http.client.utils.URIBuilder;
 import java.lang.Object.*;
-import java.net.MalformedURLException;
-import java.net.URI;
+
 import java.net.URISyntaxException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
@@ -41,24 +40,7 @@ public class EndPointSparqlServiceImpl extends RemoteServiceServlet
                builder.setParameter("format",param2);
         try {
             url=builder.build().toString();
-                    /*
-                
-                       UrlBuilder builder = new UrlBuilder();
-                       builder.setHost("ginseng.unice.fr");
-                       builder.setPort(9000);
-                       builder.setPath("/sparql"); 
                    
-                     builder.setParameter("query","select ?type (count(*) as ?c)" 
-                             + " where {?x a ?type}"
-                             + " group by ?type"
-                             + " order by desc(?c)"); 
-                            
-                       builder.setParameter("query",param1);
-                       builder.setParameter("format",param2);
-                    // builder.setParameter("format","json" );
-                     
-                      return builder.toString();
-                  */
         } catch (URISyntaxException ex) {
             Logger.getLogger(EndPointSparqlServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
