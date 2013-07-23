45eaa6463ca8bee70c55ccbe5286a9f05b54b6c5
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2013-07-23 06:04:38 +0000

    22 juillet 2013
    git-svn-id: svn+ssh://svn.creatis.insa-lyon.fr/svn/vip/Portal/trunk@4003 97043df2-9288-40f0-b43d-ea39296b91c7

---
+++ vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/QueryMakerTab.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.creatis.vip.query.client.view.monitor;
+import com.smartgwt.client.types.Overflow;
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.tab.Tab;
+import com.smartgwt.client.widgets.layout.VLayout;
+import fr.insalyon.creatis.vip.query.client.view.QueryConstants;
+import fr.insalyon.creatis.vip.query.client.view.monitor.QueryLayout;
+
+
+ import fr.insalyon.creatis.vip.query.client.view.monitor.CreateQuery;
+
+/**
+ *
+ * @author Boujelben
+ */
+public class QueryMakerTab extends Tab {
+    
+    private final QueryLayout queryLayout;
+    private final CreateQuery createQuery;
+    protected VLayout vLayout;
+    
+    public QueryMakerTab() {
+        this.setTitle(Canvas.imgHTML(QueryConstants.ICON_QUERYMAKER) + "Query Maker");
+        this.setID(QueryConstants.TAB_QUERYMAKER);
+        this.setCanClose(true);
+        this.setAttribute("paneMargin", 0);
+       
+        
+        vLayout = new VLayout();
+        vLayout.setMargin(5);
+        //vLayout.setWidth100();
+       // vLayout.setHeight100();
+        
+        
+        this.setPane(vLayout);
+        
+        
+        
+        queryLayout=new QueryLayout();
+       
+        createQuery=new CreateQuery();
+       
+        
+        vLayout.addMember(queryLayout);
+        vLayout.addMember(createQuery);
+   
+        
+        
+     }
+    
+     public void setQuery(boolean test,String name, String description, String body) {
+        createQuery.setQuery(test, name, description, body);     
+    }
+     public Long getVersionID(){
+         return queryLayout.getVersionID();
+     }
+    
+    public void loadData(){
+         queryLayout.loadData();
+     }
+    
+}
+
