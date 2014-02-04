eef6a7daa5d27099ff994521c08fbd5e111c608f
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-02-04 17:06:37 +0100

    icon for attribut and icon for class

--- Portal/vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/GinsengTreeNode.java
+++ Portal/vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/GinsengTreeNode.java
@@ -8,11 +8,29 @@ import com.smartgwt.client.widgets.tree.TreeNode;
 
 /**
  *
- * @author nouha
+ * @author Nouha Boujelben
  */
 public class GinsengTreeNode extends TreeNode {
 
-    public GinsengTreeNode(String Id, String reportsTo, String name, String type, boolean isOpen, String restriction,boolean select,String value ) {
+    public GinsengTreeNode(String Id, String reportsTo, String name,String propName, String icon, String type, boolean isOpen, String restriction,boolean select,String value,boolean isFolder,boolean open ) {
+        setAttribute("Id", Id);
+        setAttribute("ReportsTo", reportsTo);
+        setAttribute("Name", name);
+        setAttribute("PropName", propName);
+        setAttribute("icon", icon);
+        setAttribute("Type", type);
+        setAttribute("isOpen", isOpen);
+        setAttribute("Restriction", restriction);
+        setAttribute("Select", select);
+        setAttribute("Value", value);
+        setAttribute("isSelected", select);
+        setAttribute("isFolder",isFolder );
+         setAttribute("open", open);
+       
+
+    }
+    
+     public GinsengTreeNode(String Id, String reportsTo, String name, String type, boolean isOpen, String restriction,boolean select,String value) {
         setAttribute("Id", Id);
         setAttribute("ReportsTo", reportsTo);
         setAttribute("Name", name);
@@ -20,8 +38,10 @@ public class GinsengTreeNode extends TreeNode {
         setAttribute("isOpen", isOpen);
         setAttribute("Restriction", restriction);
         setAttribute("Select", select);
-         setAttribute("Value", value);
+        setAttribute("Value", value);
         setAttribute("isSelected", select);
+
+        
 
     }
 }
