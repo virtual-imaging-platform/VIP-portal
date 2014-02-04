86dccb1223419b754c85fef9aa8abbc52beffcf3
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-02-04 17:18:21 +0100

    2173

--- Portal/vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/QueryExplorerTab.java
+++ Portal/vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/QueryExplorerTab.java
@@ -4,38 +4,26 @@
  */
 package fr.insalyon.creatis.vip.query.client.view.monitor;
 
-
 import com.smartgwt.client.widgets.Canvas;
-import com.smartgwt.client.widgets.IButton;
-import com.smartgwt.client.widgets.Img;
-import com.smartgwt.client.widgets.events.ClickEvent;
-import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
-import com.smartgwt.client.widgets.layout.VLayout;
 import com.smartgwt.client.widgets.tab.Tab;
-import com.smartgwt.client.widgets.tab.TabSet;
-import com.smartgwt.client.widgets.tab.events.CloseClickHandler;
-import com.smartgwt.client.widgets.tab.events.TabCloseClickEvent;
-import com.smartgwt.client.widgets.tab.events.TabTitleChangedEvent;
-import com.smartgwt.client.widgets.tab.events.TabTitleChangedHandler;
 import fr.insalyon.creatis.vip.query.client.view.QueryConstants;
 
 /**
  *
- * @author nouha
+ * @author Nouha Boujelben
  */
 public class QueryExplorerTab extends Tab {
 
     private final CheckboxTree checkboxTree;
     private final CheckboxTreeRestriction checkboxTreeRestriction;
-    
 
     public QueryExplorerTab() {
 
         checkboxTree = new CheckboxTree();
-        checkboxTreeRestriction=new CheckboxTreeRestriction();
- 
-        HLayout layout=new HLayout();
+        checkboxTreeRestriction = new CheckboxTreeRestriction();
+
+        HLayout layout = new HLayout();
         this.setTitle(Canvas.imgHTML(QueryConstants.ICON_EXPLORE) + "Query Explorer");
         this.setID(QueryConstants.TAB_QUERYEXPLORER);
         this.setCanClose(true);
@@ -44,34 +32,35 @@ public class QueryExplorerTab extends Tab {
         layout.addMember(checkboxTree);
         layout.addMember(checkboxTreeRestriction);
         this.setPane(layout);
+        checkboxTree.loadData();
 
     }
 
     public CheckboxTree getCheckboxTree() {
         return checkboxTree;
     }
-    
-    public String getType(){
-    
+
+    public String getType() {
+
         return checkboxTree.getType();
-       
+
     }
-     public void setForm(){
-        checkboxTreeRestriction.setForm();
-     }
-     
-      public String getName(){
-    
+
+    public void setForm(String restriction) {
+        checkboxTreeRestriction.setForm(restriction);
+    }
+
+    public String getName() {
+
         return checkboxTree.getName();
-       
+
     }
-      
-      public void addRestriction(String value) {
-       checkboxTree.addRestriction(value);
+
+    public void addRestriction(String value) {
+        checkboxTree.addRestriction(value);
     }
-      
-    public void refrechGrid(){
-         checkboxTree.refrechGrid();
+
+    public void refrechGrid() {
+        checkboxTree.refrechGrid();
     }
-    
 }
