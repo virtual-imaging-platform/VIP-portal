ed0e5bff145999daab147b0690b0edc22b7c44a3
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-02-04 17:17:00 +0100

    some arrangement

--- Portal/vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/HistoryToolStrip.java
+++ Portal/vip-query/src/main/java/fr/insalyon/creatis/vip/query/client/view/monitor/HistoryToolStrip.java
@@ -24,12 +24,11 @@ import java.util.List;
 
 /**
  *
- * @author Boujelben
+ * @author Nouha Boujelben
  */
 public class HistoryToolStrip extends ToolStrip {
 
     private ModalWindow modal;
-    String n = null;
 
     public HistoryToolStrip(ModalWindow modal) {
 
@@ -74,165 +73,6 @@ public class HistoryToolStrip extends ToolStrip {
             }
         }));
 
-        //Kill Simulations Button
-        /*
-         this.addButton(WidgetUtil.getToolStripButton("Kill Query Execution",
-         QueryConstants.ICON_KILL, null, new ClickHandler() {
-
-         @Override
-         public void onClick(ClickEvent event) {
-         SC.ask("Do you really want to kill the selected running query?", new BooleanCallback() {
-
-         @Override
-         public void execute(Boolean value) {
-         if (value) {
-         //    killSimulations();
-         }
-         }
-                   
-         });
-         }
-         }));
-         * 
-
-         // Clean Simulations Button
-         this.addButton(WidgetUtil.getToolStripButton("Clean Query",
-         QueryConstants.ICON_CLEAN, null, new ClickHandler() {
-
-         @Override
-         public void onClick(ClickEvent event) {
-         SC.confirm("Do you really want to clean the selected completed/killed Query?", new BooleanCallback() {
-
-         @Override
-         public void execute(Boolean value) {
-         if (value) {
-         //cleanSimulations();
-         }
-         }
-         });
-         }
-         }));
-
-         // if (CoreModule.user.isSystemAdministrator()) {
-
-         // Purge Simulations
-           
-
-          
-         /**
-         * Sends a request to kill the selected running simulations
-         *
-         */
-        /*private void killSimulations() {
-
-         ListGridRecord[] records = getSimulationsTab().getGridSelection();
-         List<String> simulationIDs = new ArrayList<String>();
-
-         for (ListGridRecord record : records) {
-         SimulationRecord data = (SimulationRecord) record;
-         SimulationStatus status = SimulationStatus.valueOf(data.getStatus());
-
-         if (status == SimulationStatus.Running) {
-         simulationIDs.add(data.getSimulationId());
-         }
-         }
-
-         WorkflowServiceAsync service = WorkflowService.Util.getInstance();
-         final AsyncCallback<Void> callback = new AsyncCallback<Void>() {
-
-         @Override
-         public void onFailure(Throwable caught) {
-         modal.hide();
-         Layout.getInstance().setWarningMessage("Unable to kill simulations:<br />" + caught.getMessage());
-         }
-
-         @Override
-         public void onSuccess(Void result) {
-         modal.hide();
-         getSimulationsTab().loadData();
-         }
-         };
-         service.killSimulations(simulationIDs, callback);
-         modal.show("Sending killing signal to selected simulations...", true);
-         }
-
-         /**
-         * Sends a request to clean the selected completed/killed simulations
-         *
-         */
-        /*
-         private void cleanSimulations() {
-
-         ListGridRecord[] records = getSimulationsTab().getGridSelection();
-         List<String> simulationIDs = new ArrayList<String>();
-
-         for (ListGridRecord record : records) {
-         SimulationRecord data = (SimulationRecord) record;
-         SimulationStatus status = SimulationStatus.valueOf(data.getStatus());
-
-         if (status == SimulationStatus.Completed
-         || status == SimulationStatus.Killed) {
-
-         simulationIDs.add(data.getSimulationId());
-         }
-         }
-
-         WorkflowServiceAsync service = WorkflowService.Util.getInstance();
-         final AsyncCallback<Void> callback = new AsyncCallback<Void>() {
-
-         @Override
-         public void onFailure(Throwable caught) {
-         modal.hide();
-         Layout.getInstance().setWarningMessage("Unable to clean simulations:<br />" + caught.getMessage());
-         }
-
-         @Override
-         public void onSuccess(Void result) {
-         modal.hide();
-         getSimulationsTab().loadData();
-         }
-         };
-         service.cleanSimulations(simulationIDs, callback);
-         modal.show("Cleaning selected simulations...", true);
-         }
-
-         /**
-         * Sends a request to purge the selected cleaned simulations
-         *
-         */
-        /*private void purgeSimulations() {
-
-         ListGridRecord[] records = getSimulationsTab().getGridSelection();
-         List<String> simulationIDs = new ArrayList<String>();
-
-         for (ListGridRecord record : records) {
-         SimulationRecord data = (SimulationRecord) record;
-         SimulationStatus status = SimulationStatus.valueOf(data.getStatus());
-
-         if (status == SimulationStatus.Cleaned) {
-         simulationIDs.add(data.getSimulationId());
-         }
-         }
-
-         WorkflowServiceAsync service = WorkflowService.Util.getInstance();
-         final AsyncCallback<Void> callback = new AsyncCallback<Void>() {
-
-         @Override
-         public void onFailure(Throwable caught) {
-         modal.hide();
-         Layout.getInstance().setWarningMessage("Unable to purge simulations:<br />" + caught.getMessage());
-         }
-
-         @Override
-         public void onSuccess(Void result) {
-         modal.hide();
-         getSimulationsTab().loadData();
-         }
-         };
-         service.purgeSimulations(simulationIDs, callback);
-         modal.show("Purging selected simulations...", true);
-         }
-         * */
     }
 
     private void purgeQuery() {
@@ -252,16 +92,10 @@ public class HistoryToolStrip extends ToolStrip {
                 @Override
                 public void onSuccess(Void result) {
                     getQueryHistoryTab().loadData();
-
                 }
             };
             QueryService.Util.getInstance().removeQueryExecution(id, callback);
-
-
-
         }
-
-
     }
 
     private QueryHistoryTab getQueryHistoryTab() {
