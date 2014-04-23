2dac3d037a32d8b87fd4cf5ccc31778dbd6eb437
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-04-23 18:51:34 +0200

    #2351

--- Portal/vip-n4u/src/main/java/fr/insalyon/creatis/vip/n4u/server/FileProcessServiceImpl.java
+++ Portal/vip-n4u/src/main/java/fr/insalyon/creatis/vip/n4u/server/FileProcessServiceImpl.java
@@ -54,23 +54,20 @@ public class FileProcessServiceImpl extends fr.insalyon.creatis.vip.core.server.
     //local dir
     public List<List<String>> fileTraitement(String param1) {
         String localFilePat = null;
-       try {
-            
-                localFilePat = CoreUtil.getGRIDAClient().getRemoteFile(DataManagerUtil.parseBaseDir(getSessionUser(),param1), System.getProperty("user.home"));
-                } catch (CoreException ex) {
-                   logger.error(ex);
-                   logger.error(param1);
-               
-                }
-            catch (DataManagerException ex) {
-               logger.error(ex);
-            }
-         catch (GRIDAClientException ex) {
-             logger.error(ex);
-        }
-       
-        List<List<String>> result = new ArrayList<List<String>>();
+        try {
 
+            localFilePat = CoreUtil.getGRIDAClient().getRemoteFile(DataManagerUtil.parseBaseDir(getSessionUser(), param1), System.getProperty("user.home"));
+        } catch (CoreException ex) {
+            logger.error(ex);
+            logger.error(param1);
+
+        } catch (DataManagerException ex) {
+            logger.error(ex);
+        } catch (GRIDAClientException ex) {
+            logger.error(ex);
+        }
+
+        List<List<String>> result = new ArrayList<List<String>>();
         List<String> inputsDir = new ArrayList<String>();
         List<String> outputsDir = new ArrayList<String>();
         try {
@@ -97,186 +94,177 @@ public class FileProcessServiceImpl extends fr.insalyon.creatis.vip.core.server.
         result.add(outputsDir);
         return result;
     }
-    
-    
-    
-    
-    
+
     //retourne nombre d'entree ainsi que le nombre des entree de type file
-    
-     public int[] fileJobTraitement(String jobFile,String expressFile) {
-         int[]result=new int[2];
-          int nb=0;
-       String localFilePat = null;
-       String localFilePathExpress = null;
-      
-       
-       try {
-            
-                localFilePat = CoreUtil.getGRIDAClient().getRemoteFile(DataManagerUtil.parseBaseDir(getSessionUser(),jobFile), System.getProperty("user.home"));
-                } catch (CoreException ex) {
-                   logger.error(ex);
-                   
-               
-                }
-            catch (DataManagerException ex) {
-               logger.error(ex);
-            }
-         catch (GRIDAClientException ex) {
-             logger.error(ex);
-        }
-     
-      
+    public int[] fileJobTraitement(String jobFile, String expressFile) {
+        int[] result = new int[2];
+        int nb = 0;
+        String localFilePat = null;
+        String localFilePathExpress = null;
+
+
         try {
-            
-            
-                    localFilePathExpress = CoreUtil.getGRIDAClient().getRemoteFile(DataManagerUtil.parseBaseDir(getSessionUser(),expressFile), System.getProperty("user.home"));
-                } catch (CoreException ex) {
-                   logger.error(ex);
-                   
-               
-                }
-            catch (DataManagerException ex) {
-               logger.error(ex);
-            }
-         catch (GRIDAClientException ex) {
-             logger.error(ex);
+
+            localFilePat = CoreUtil.getGRIDAClient().getRemoteFile(DataManagerUtil.parseBaseDir(getSessionUser(), jobFile), System.getProperty("user.home"));
+        } catch (CoreException ex) {
+            logger.error(ex);
+
+
+        } catch (DataManagerException ex) {
+            logger.error(ex);
+        } catch (GRIDAClientException ex) {
+            logger.error(ex);
         }
-      
-      
+
         try {
-          Scanner scanner = new Scanner(new FileInputStream(localFilePat));
-           //Scanner scanner = new Scanner(new FileInputStream("/home/nouha/Siena.jobs"));
-            
-               String ligne = scanner.nextLine();
-               while(ligne.substring(0, 1).equals("#")){
-               ligne = scanner.nextLine();
-               }
-                
-               StringTokenizer strok = new StringTokenizer(ligne,"\t");
-              
-               nb = strok.countTokens(); 
-              result[0]=nb;
-               
-        Scanner scanner2 = new Scanner(new FileInputStream(localFilePathExpress));
-       // Scanner scanner2 = new Scanner(new FileInputStream("/home/nouha/Siena.express"));
-         
-        while( scanner2.hasNext()){
-          String readLigne = scanner2.nextLine();   
-          if(readLigne.contains("ExplicitFiles")){
-          int fileNumber=Integer.parseInt(readLigne.substring(14));
-          result[1]=fileNumber;
-          }
-              
-        
+            localFilePathExpress = CoreUtil.getGRIDAClient().getRemoteFile(DataManagerUtil.parseBaseDir(getSessionUser(), expressFile), System.getProperty("user.home"));
+        } catch (CoreException ex) {
+            logger.error(ex);
+
+
+        } catch (DataManagerException ex) {
+            logger.error(ex);
+        } catch (GRIDAClientException ex) {
+            logger.error(ex);
         }
+
+
+        try {
+            Scanner scanner = new Scanner(new FileInputStream(localFilePat));
+            //Scanner scanner = new Scanner(new FileInputStream("/home/nouha/Siena.jobs"));
+
+            String ligne = scanner.nextLine();
+            while (ligne.substring(0, 1).equals("#")) {
+                ligne = scanner.nextLine();
+            }
+
+            StringTokenizer strok = new StringTokenizer(ligne, "\t");
+
+            nb = strok.countTokens();
+            result[0] = nb;
+
+            Scanner scanner2 = new Scanner(new FileInputStream(localFilePathExpress));
+            // Scanner scanner2 = new Scanner(new FileInputStream("/home/nouha/Siena.express"));
+
+            while (scanner2.hasNext()) {
+                String readLigne = scanner2.nextLine();
+                if (readLigne.contains("ExplicitFiles")) {
+                    int fileNumber = Integer.parseInt(readLigne.substring(14));
+                    result[1] = fileNumber;
+                }
+
+
+            }
 
         } catch (IOException ex) {
             logger.error(ex);
         }
-      
+
         return result;
     }
+
    
-     private  java.sql.Timestamp getCurrentTimeStamp() {
+
+    @Override
+    public void generateScriptFile(ArrayList listInput, ArrayList listOutput, String wrapperScriptPath, String scriptFile, String applicationName, String applicationLocation, String description) {
+        String applicationRealLocation = null;
+        try {
+            applicationRealLocation = DataManagerUtil.parseBaseDir(getSessionUser(), applicationLocation);
+        } catch (CoreException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (DataManagerException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //create floder to genrate file
+        final File homeDir = new File(Server.getInstance().getApplicationFilesRepository());
+        File theDir = null;
+        try {
+            theDir = new File(homeDir, applicationName + "/" + getSessionUser().getFolder() + "/" + getCurrentTimeStamp());
+            theDir.mkdirs();
+        } catch (CoreException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        String dir = theDir.getAbsolutePath();
+        try {
+            new Velocity().wrapperScriptFile(listInput, listOutput, applicationName, scriptFile, applicationRealLocation, dir);
+        } catch (N4uException ex) {
+            logger.error(ex);
+        }
+    }
+
+    @Override
+    public String generateGwendiaFile(ArrayList listInput, ArrayList listOutput, String wrapperScriptPath, String scriptFile, String applicationName, String applicationLocation, String description) {
+        String applicationRealLocation = null;
+        try {
+            applicationRealLocation = DataManagerUtil.parseBaseDir(getSessionUser(), applicationLocation);
+        } catch (CoreException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (DataManagerException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //create floder to genrate file
+        final File homeDir = new File(Server.getInstance().getApplicationFilesRepository());
+        File theDir = null;
+        try {
+            theDir = new File(homeDir, applicationName + "/" + getSessionUser().getFolder() + "/" + getCurrentTimeStamp());
+            theDir.mkdirs();
+        } catch (CoreException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+
+        String dir = theDir.getAbsolutePath();
+       
+            return new Velocity().gwendiaFile(listInput, listOutput, applicationName, description, applicationRealLocation, dir);
+      
+    }
+
+    @Override
+    public void generateGaswFile(ArrayList listInput, ArrayList listOutput, String wrapperScriptPath, String scriptFile, String applicationName, String applicationLocation, String description) {
+        String applicationRealLocation = null;
+        try {
+            applicationRealLocation = DataManagerUtil.parseBaseDir(getSessionUser(), applicationLocation);
+        } catch (CoreException e) {
+            logger.error(e);
+        } catch (DataManagerException ex) {
+            logger.error(ex);
+        }
+
+        //create floder to genrate file
+        final File homeDir = new File(Server.getInstance().getApplicationFilesRepository());
+        File theDir = null;
+        try {
+            theDir = new File(homeDir, applicationName + "/" + getSessionUser().getFolder() + "/" + getCurrentTimeStamp());
+            theDir.mkdirs();
+
+        } catch (CoreException ex) {
+            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // if the directory does not exist, create it
+
+
+        String dir = theDir.getAbsolutePath();
+        try {
+            new Velocity().gassFile(listInput, listOutput, applicationName, wrapperScriptPath, applicationRealLocation, dir);
+        } catch (N4uException ex) {
+            logger.error(ex);
+        }
+    }
+
+    public String getApplicationClasse() {
+        return Server.getInstance().getApplicationN4uClasse();
+    }
+    
+    private java.sql.Timestamp getCurrentTimeStamp() {
 
         java.util.Date today = new java.util.Date();
         return new java.sql.Timestamp(today.getTime());
 
     }
-
-    @Override
-    public void generateScriptFile(ArrayList listInput, ArrayList listOutput, String wrapperScriptPath, String scriptFile, String applicationName, String applicationLocation, String description) {
-        String applicationRealLocation=null;
-        try {
-            applicationRealLocation=DataManagerUtil.parseBaseDir(getSessionUser(),applicationLocation);
-        } catch (CoreException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (DataManagerException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        //create floder to genrate file
-        final File homeDir = new File(Server.getInstance().getApplicationFilesRepository());
-        File theDir = null;
-        try {
-            theDir = new File(homeDir,applicationName+"/"+getSessionUser().getFolder()+"/"+getCurrentTimeStamp());
-            theDir.mkdirs();
-        } catch (CoreException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        
-       String dir=theDir.getAbsolutePath();
-      
-        new Velocity().wrapperScriptFile(listInput,listOutput,applicationName, scriptFile,applicationRealLocation,dir);
-    }
-
-    @Override
-    public String generateGwendiaFile(ArrayList listInput, ArrayList listOutput, String wrapperScriptPath, String scriptFile, String applicationName, String applicationLocation, String description) {
-           String applicationRealLocation=null;
-        try {
-            applicationRealLocation=DataManagerUtil.parseBaseDir(getSessionUser(),applicationLocation);
-        } catch (CoreException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (DataManagerException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        //create floder to genrate file
-        final File homeDir = new File(Server.getInstance().getApplicationFilesRepository());
-        File theDir = null;
-        try {
-            theDir = new File(homeDir,applicationName+"/"+getSessionUser().getFolder()+"/"+getCurrentTimeStamp());
-            theDir.mkdirs();
-        } catch (CoreException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    
-        
-       String dir=theDir.getAbsolutePath();
-      
-       
-        return new Velocity().gwendiaFile(listInput,listOutput, applicationName,description,applicationRealLocation,dir);
-    }
-
-    @Override
-    public void generateGaswFile(ArrayList listInput, ArrayList listOutput, String wrapperScriptPath, String scriptFile, String applicationName, String applicationLocation, String description) {
-       String applicationRealLocation=null;
-        try {
-            applicationRealLocation=DataManagerUtil.parseBaseDir(getSessionUser(),applicationLocation);
-        } catch (CoreException e) {
-            logger.error(e);
-        } catch (DataManagerException ex) {
-           logger.error(ex);
-        }
-        
-        //create floder to genrate file
-        final File homeDir = new File(Server.getInstance().getApplicationFilesRepository());
-        File theDir = null;
-        try {
-            theDir = new File(homeDir,applicationName+"/"+getSessionUser().getFolder()+"/"+getCurrentTimeStamp());
-            theDir.mkdirs();
-            
-        } catch (CoreException ex) {
-            java.util.logging.Logger.getLogger(FileProcessServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    // if the directory does not exist, create it
-    
-        
-       String dir=theDir.getAbsolutePath();
-        new Velocity().gassFile(listInput,listOutput,applicationName,wrapperScriptPath,applicationRealLocation,dir );
-    }
-
-   
-    public String getApplicationClasse() {
-        return Server.getInstance().getApplicationN4uClasse();
-    }
-    
-    
-    
-   
-   
 }
