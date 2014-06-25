eeebea96780b37184213edf30914a14ff71ef47b
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-06-26 00:35:05 +0200

    #2372

---
+++ Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/system/setting/ManageSettingTab.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.creatis.vip.core.client.view.system.setting;
+
+
+
+import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.VLayout;
+import fr.insalyon.creatis.vip.core.client.view.CoreConstants;
+import fr.insalyon.creatis.vip.core.client.view.common.AbstractManageTab;
+
+/**
+ *
+ * @author Rafael Ferreira da Silva
+ */
+public class ManageSettingTab extends AbstractManageTab {
+
+    private SettingTermsUseLayout settingTermsUseLayout;
+  
+
+    public ManageSettingTab() {
+
+        super(CoreConstants.ICON_SETTING, CoreConstants.APP_SETTING, CoreConstants.TAB_MANAGE_SETTING);
+       
+        settingTermsUseLayout = new SettingTermsUseLayout();
+       
+        
+        HLayout hLayout = new HLayout(5);
+        
+        VLayout leftLayout = new VLayout(14);
+        leftLayout.setWidth(350);
+        leftLayout.setHeight100();
+        leftLayout.addMember(settingTermsUseLayout);
+        VLayout rightLayout = new VLayout(14);
+        rightLayout.setWidth(350);
+        rightLayout.setHeight100();
+        
+        hLayout.addMember(leftLayout);
+        hLayout.addMember(rightLayout);
+        
+        vLayout.setPadding(10);     
+        vLayout.addMember(hLayout);
+        
+    }
+
+}
