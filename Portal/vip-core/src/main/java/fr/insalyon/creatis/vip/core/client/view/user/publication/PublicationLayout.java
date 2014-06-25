1c6c609849f58b73cdb2d4ed8f121ad589dc86f5
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-06-26 00:37:15 +0200

    #2407

--- Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/user/publication/PublicationLayout.java
+++ Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/user/publication/PublicationLayout.java
@@ -41,13 +41,18 @@ import java.util.List;
  */
 public class PublicationLayout extends VLayout {
 
+    PublicationGrid publicationGrid;
     private final ModalWindow modal;
     private ListGrid grid;
-    private HLayout rollOverCanvas;
-    private ListGridRecord rollOverRecord;
-    private DetailViewer detailViewer;
-    DataSource ds;
+    private static PublicationLayout instance;
     boolean state = true;
+
+    public static PublicationLayout getInstance() {
+        if (instance == null) {
+            instance = new PublicationLayout();
+        }
+        return instance;
+    }
 
     public PublicationLayout() {
 
@@ -101,117 +106,11 @@ public class PublicationLayout extends VLayout {
 
     private void configureGrid() {
 
-        ds = new Data();
 
-        grid = new ListGrid() {
-            @Override
-            protected Canvas getRollOverCanvas(Integer rowNum, Integer colNum) {
-                rollOverRecord = this.getRecord(rowNum);
 
-                if (rollOverCanvas == null) {
-                    rollOverCanvas = new HLayout(3);
-                    rollOverCanvas.setSnapTo("TR");
-                    rollOverCanvas.setWidth(50);
-                    rollOverCanvas.setHeight(22);
+        publicationGrid = new PublicationGrid();
+        grid = publicationGrid.grid;
 
-                    final ImgButton loadImg = getImgButton(CoreConstants.ICON_EDIT, "Edit");
-                    loadImg.addClickHandler(new ClickHandler() {
-                        @Override
-                        public void onClick(ClickEvent event) {
-                            edit(rollOverRecord.getAttribute("id"), rollOverRecord.getAttribute("title"),
-                                    rollOverRecord.getAttribute("type"),
-                                    rollOverRecord.getAttribute("typeName"), rollOverRecord.getAttribute("authors"), rollOverRecord.getAttribute("date"), rollOverRecord.getAttribute("doi"));
-                        }
-                    });
-                    final ImgButton deleteImg = getImgButton(CoreConstants.ICON_DELETE, "Delete");
-                    deleteImg.addClickHandler(new ClickHandler() {
-                        @Override
-                        public void onClick(ClickEvent event) {
-                            final String id = rollOverRecord.getAttribute("id");
-                            SC.ask("Do you really want to remove the publication \""
-                                    + rollOverRecord.getAttribute("title") + " ?", new BooleanCallback() {
-                                        @Override
-                                        public void execute(Boolean value) {
-                                            if (value) {
-                                                remove(Long.valueOf(id));
-                                            }
-                                        }
-                                    });
-                        }
-                    });
-
-                    if (CoreModule.user.getLevel() == UserLevel.Administrator) {
-                        rollOverCanvas.addMember(loadImg);
-                        rollOverCanvas.addMember(deleteImg);
-                    }
-
-                }
-                return rollOverCanvas;
-            }
-
-            private ImgButton getImgButton(String imgSrc, String prompt) {
-                ImgButton button = new ImgButton();
-                button.setShowDown(false);
-                button.setShowRollOver(false);
-                button.setLayoutAlign(Alignment.CENTER);
-                button.setSrc(imgSrc);
-                button.setPrompt(prompt);
-                button.setHeight(16);
-                button.setWidth(16);
-                return button;
-            }
-
-            @Override
-            protected Canvas getCellHoverComponent(Record record, Integer rowNum, Integer colNum) {
-
-                detailViewer = new DetailViewer();
-                detailViewer.setWidth(400);
-                detailViewer.setFields(
-                        new DetailViewerField("title", "Title"),
-                        new DetailViewerField("type", "Type"),
-                        new DetailViewerField("typeName", "Journal, Conference or Book Name"),
-                        new DetailViewerField("authors", "Authors"),
-                        new DetailViewerField("date", "Date"),
-                        new DetailViewerField("doi", "Doi"));
-                detailViewer.setData(new Record[]{record});
-
-                return detailViewer;
-            }
-        };
-        grid.setWidth100();
-        grid.setHeight100();
-        grid.setShowRollOverCanvas(true);
-        grid.setShowEmptyMessage(true);
-        grid.setShowAllRecords(false);
-        grid.setShowRowNumbers(true);
-        grid.setCanHover(true);
-        grid.setShowHover(true);
-        grid.setShowHoverComponents(true);
-        grid.setShowRowNumbers(true);
-        grid.setEmptyMessage("<br>No data available.");
-        grid.setFilterOnKeypress(true);
-        grid.setDataSource(ds);
-        grid.setAutoFetchData(Boolean.TRUE);
-        ListGridField id = new ListGridField("id", "ID");
-        grid.setFields(id, new ListGridField("title", "Title"),
-                new ListGridField("type", "Type"),
-                new ListGridField("typeName", "Journal, Conference or Book Name"),
-                new ListGridField("authors", "Authors"),
-                new ListGridField("date", "Date"));
-
-        id.setHidden(true);
-        grid.setSortField("title");
-        grid.setSortDirection(SortDirection.ASCENDING);
-        grid.addCellClickHandler(new CellClickHandler() {
-            @Override
-            public void onCellClick(CellClickEvent event) {
-                if (CoreModule.user.getLevel() == UserLevel.Administrator) {
-                    edit(event.getRecord().getAttribute("id"), event.getRecord().getAttribute("title"),
-                            event.getRecord().getAttribute("type"),
-                            event.getRecord().getAttribute("typeName"), event.getRecord().getAttribute("authors"), event.getRecord().getAttribute("date"), event.getRecord().getAttribute("doi"));
-                }
-            }
-        });
         this.addMember(grid);
     }
 
@@ -231,14 +130,14 @@ public class PublicationLayout extends VLayout {
                     dataList.add(new PublicationRecord(pub.getId(), pub.getTitle(), pub.getType(), pub.getTypeName(), pub.getDate(), pub.getAuthors(), pub.getDoi(), pub.getVipAuthor()));
                 }
                 grid.setData(dataList.toArray(new PublicationRecord[]{}));
-                ds.setTestData(dataList.toArray(new PublicationRecord[]{}));
+                publicationGrid.ds.setTestData(dataList.toArray(new PublicationRecord[]{}));
             }
         };
         modal.show("Loading Publications...", true);
         ConfigurationService.Util.getInstance().getPublications(callback);
     }
 
-    private void edit(String id, String title, String type, String typeName, String authors, String date, String doi) {
+    protected static void edit(String id, String title, String type, String typeName, String authors, String date, String doi) {
 
         PublicationTab pubTab = (PublicationTab) Layout.getInstance().
                 getTab(CoreConstants.TAB_PUBLICATION);
