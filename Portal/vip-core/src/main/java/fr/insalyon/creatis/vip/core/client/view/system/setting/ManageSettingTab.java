532f3bd5e83e8285aeca59cb81f57d98505383f6
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2014-07-03 16:12:52 +0200

    #2372

--- Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/system/setting/ManageSettingTab.java
+++ Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/system/setting/ManageSettingTab.java
@@ -4,8 +4,6 @@
  */
 package fr.insalyon.creatis.vip.core.client.view.system.setting;
 
-
-
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.VLayout;
 import fr.insalyon.creatis.vip.core.client.view.CoreConstants;
@@ -18,17 +16,16 @@ import fr.insalyon.creatis.vip.core.client.view.common.AbstractManageTab;
 public class ManageSettingTab extends AbstractManageTab {
 
     private SettingTermsUseLayout settingTermsUseLayout;
-  
 
     public ManageSettingTab() {
 
         super(CoreConstants.ICON_SETTING, CoreConstants.APP_SETTING, CoreConstants.TAB_MANAGE_SETTING);
-       
+
         settingTermsUseLayout = new SettingTermsUseLayout();
-       
-        
+
+
         HLayout hLayout = new HLayout(5);
-        
+
         VLayout leftLayout = new VLayout(14);
         leftLayout.setWidth(350);
         leftLayout.setHeight100();
@@ -36,13 +33,12 @@ public class ManageSettingTab extends AbstractManageTab {
         VLayout rightLayout = new VLayout(14);
         rightLayout.setWidth(350);
         rightLayout.setHeight100();
-        
+
         hLayout.addMember(leftLayout);
         hLayout.addMember(rightLayout);
-        
-        vLayout.setPadding(10);     
-        vLayout.addMember(hLayout);
-        
-    }
 
+        vLayout.setPadding(10);
+        vLayout.addMember(hLayout);
+
+    }
 }
--- Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/system/setting/SettingTermsUseLayout.java
+++ Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/system/setting/SettingTermsUseLayout.java
@@ -4,12 +4,16 @@
  */
 package fr.insalyon.creatis.vip.core.client.view.system.setting;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
+import fr.insalyon.creatis.vip.core.client.bean.TermsOfUse;
+import fr.insalyon.creatis.vip.core.client.rpc.ConfigurationService;
 import fr.insalyon.creatis.vip.core.client.view.CoreConstants;
 import fr.insalyon.creatis.vip.core.client.view.common.AbstractFormLayout;
+import fr.insalyon.creatis.vip.core.client.view.layout.Layout;
 import fr.insalyon.creatis.vip.core.client.view.util.WidgetUtil;
 import java.util.Arrays;
 
@@ -20,24 +24,41 @@ import java.util.Arrays;
 public class SettingTermsUseLayout extends AbstractFormLayout {
 
     private IButton updateTermsUse;
-    
+
     public SettingTermsUseLayout() {
-        
-        
+
+
         super("350", "100");
         addTitle("Terms Of Use", CoreConstants.ICON_TERMS_USE);
         configure();
     }
-    
+
     private void configure() {
         updateTermsUse = WidgetUtil.getIButton("Terms of Use have been modified", CoreConstants.ICON_EDIT,
                 new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                
+
+                ConfigurationService.Util.getInstance().addTermsUse(new TermsOfUse("the text"), getCallback("add"));
+
+
             }
         });
         updateTermsUse.setWidth(200);
-        addButtons(updateTermsUse);        
+        addButtons(updateTermsUse);
+    }
+
+    private AsyncCallback<Void> getCallback(final String text) {
+
+        return new AsyncCallback<Void>() {
+            @Override
+            public void onFailure(Throwable caught) {
+                Layout.getInstance().setWarningMessage("Unable to " + text + " terms of use:<br />" + caught.getMessage());
+            }
+
+            @Override
+            public void onSuccess(Void result) {
+            }
+        };
     }
 }
