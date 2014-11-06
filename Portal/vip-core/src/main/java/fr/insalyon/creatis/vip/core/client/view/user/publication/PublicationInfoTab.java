322ee075375e656d99cc64864aacd87594cbddf3
Tristan Glatard <tristan.glatard@creatis.insa-lyon.fr>
2014-11-06 15:31:40 -0500

    #2460

--- Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/user/publication/PublicationInfoTab.java
+++ Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/user/publication/PublicationInfoTab.java
@@ -17,8 +17,17 @@ public class PublicationInfoTab extends AbstractFormLayout{
 
         super("100%", "50");
         addTitle("", CoreConstants.ICON_INFO);
-        addMember(WidgetUtil.getLabel("<b>Please list here the references of the publications that you made using VIP. These references may"
-                + " be used by the VIP team to justify the use of computing and storage resources.</b>", 20));
+        addMember(WidgetUtil.getLabel(
+                "<b>Please list here the references of the publications "
+                + "that you made using VIP. These references may"
+                + " be used by the VIP team to justify the use of "
+                + "computing and storage resources. <br/><font color=\"blue\">Fr"
+                        + "</font><font color=\"white\">en</font><font "
+                        + "color=\"red\">ch</font> users: "
+                        + "France-Grilles also requires that the "
+                        + "publications made using their infrastructure are "
+                        + "registered in <a href=\"http://hal.archives-ouvertes.fr\">HAL</a> "
+                        + "(only authors can do that, we cannot do it for you).</b>", 20));
         
     }
     
