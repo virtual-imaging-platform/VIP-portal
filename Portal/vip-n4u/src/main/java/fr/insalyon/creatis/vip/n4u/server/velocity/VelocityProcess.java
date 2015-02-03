743fb0afcdb038fee9c73dbfa83e201b3c32565d
Nouha Boujelben <nouha.boujelben@creatis.insa-lyon.fr>
2015-02-03 17:49:07 +0100

    #2524

--- Portal/vip-n4u/src/main/java/fr/insalyon/creatis/vip/n4u/server/velocity/VelocityProcess.java
+++ Portal/vip-n4u/src/main/java/fr/insalyon/creatis/vip/n4u/server/velocity/VelocityProcess.java
@@ -15,7 +15,7 @@ public interface VelocityProcess {
 
     public void gaswFile(Map<Integer, Map> listInput, List<Map> listOutput, String applicationName, String wrapperScriptPath, String applicationLocation, String dir, String date, String sandbox, String environementFile, String extensionFileValue, String executableSandbox) throws VelocityException;
 
-    public void wrapperScriptFile(Map<Integer, Map> listInput, List<Map> listOutput, String applicationName, String scriptFile, String applicationLocation, String environementFile, String dir, String date, String mandatoryDir) throws VelocityException;
+    public void wrapperScriptFile(Map<Integer, Map> listInput, List<Map> listOutput, String applicationName, String scriptFile, String applicationLocation, String environementFile, String dir, String date, List<String> mandatoryDir) throws VelocityException;
 
-    public String gwendiaFile(Map<Integer, Map> listInput, List<Map> listOutput, String applicationName, String description, String applicationLocation, String dir, String date, String mandatoryDir) throws VelocityException;
+    public String gwendiaFile(Map<Integer, Map> listInput, List<Map> listOutput, String applicationName, String description, String applicationLocation, String dir, String date, List<String> mandatoryDir) throws VelocityException;
 }
