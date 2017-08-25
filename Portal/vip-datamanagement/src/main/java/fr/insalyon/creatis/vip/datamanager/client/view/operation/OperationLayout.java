0e0aecd880b1649a1cc2e695b966b94b95464117
Sorina Pop <sorina.pop@creatis.insa-lyon.fr>
2017-08-25 17:36:07 +0200

    Deleted unnecessary message

--- Portal/vip-datamanagement/src/main/java/fr/insalyon/creatis/vip/datamanager/client/view/operation/OperationLayout.java
+++ Portal/vip-datamanagement/src/main/java/fr/insalyon/creatis/vip/datamanager/client/view/operation/OperationLayout.java
@@ -166,7 +166,6 @@ public class OperationLayout extends VLayout {
      */
     public void addOperation(final String operationID) {
 
-        Layout.getInstance().setWarningMessage("Adding opperation ID "+operationID);
         DataManagerServiceAsync service = DataManagerService.Util.getInstance();
         AsyncCallback<PoolOperation> asyncCallback = new AsyncCallback<PoolOperation>() {
 
