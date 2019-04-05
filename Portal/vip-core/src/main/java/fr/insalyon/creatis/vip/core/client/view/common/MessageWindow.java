9af411dab440d1c7c99cab01adc59389cc96dcc9
Pascal Wassong <pwassong@sbgat328.in2p3.fr>
2019-04-05 12:00:53 +0200

    chore: remove function call only usefull for old IE (6-9).
    This call generated a warning in the console.

--- Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/common/MessageWindow.java
+++ Portal/vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/common/MessageWindow.java
@@ -4,16 +4,16 @@
  * This software is a web portal for pipeline execution on distributed systems.
  *
  * This software is governed by the CeCILL-B license under French law and
- * abiding by the rules of distribution of free software.  You can  use, 
+ * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL-B
  * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info". 
+ * "http://www.cecill.info".
  *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
- * liability. 
+ * liability.
  *
  * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
@@ -22,9 +22,9 @@
  * therefore means  that it is reserved for developers  and  experienced
  * professionals having in-depth computer knowledge. Users are therefore
  * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or 
- * data to be ensured and,  more generally, to use and operate it in the 
- * same conditions as regards security. 
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-B license and that you accept its terms.
@@ -58,14 +58,13 @@ public class MessageWindow {
     public MessageWindow(final Canvas canvas) {
 
         this.canvas = canvas;
-        
+
         panel = new HLayout();
         panel.hide();
 
         panel.setOpacity(75);
         panel.setBackgroundColor("#FFFFFF");
         panel.setBorder("1px solid #CCCCCC");
-        panel.setUseOpacityFilter(true);
         panel.setAnimateTime(1200);
         panel.addDrawHandler(new DrawHandler() {
             @Override
@@ -86,14 +85,14 @@ public class MessageWindow {
 
     /**
      * Gets the X axis position for the panel.
-     * 
+     *
      * @param canvas
-     * @return 
+     * @return
      */
     private int getPanelXPosition(Canvas canvas) {
         return (canvas.getVisibleWidth() / 2) - (panel.getWidth() / 2);
     }
-    
+
     /**
      * Removes all components from the panel.
      */
@@ -107,7 +106,7 @@ public class MessageWindow {
 
     /**
      * Creates a label and adds it to the message panel.
-     * 
+     *
      * @param title Label title
      * @param icon Label icon
      */
@@ -152,7 +151,7 @@ public class MessageWindow {
 
     /**
      * Displays a message in the panel.
-     * 
+     *
      * @param message Message to be displayed
      * @param bgColor Background color in hexadecimal
      * @param icon Message icon
