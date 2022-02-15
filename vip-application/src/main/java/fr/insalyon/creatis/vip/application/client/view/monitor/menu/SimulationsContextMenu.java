d1965efb346a2a2c003c341e75f36c24dc53b59c
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2022-02-15 09:41:26 +0100

    relaunch gate on custom gate form

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/client/view/monitor/menu/SimulationsContextMenu.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/client/view/monitor/menu/SimulationsContextMenu.java
@@ -43,6 +43,7 @@ import fr.insalyon.creatis.vip.application.client.ApplicationConstants;
 import fr.insalyon.creatis.vip.application.client.rpc.WorkflowService;
 import fr.insalyon.creatis.vip.application.client.view.common.AbstractSimulationTab;
 import fr.insalyon.creatis.vip.application.client.view.launch.LaunchTab;
+import fr.insalyon.creatis.vip.application.client.view.launch.RelaunchService;
 import fr.insalyon.creatis.vip.application.client.view.monitor.ChangeSimulationUserLayout;
 import fr.insalyon.creatis.vip.application.client.view.monitor.SimulationStatus;
 import fr.insalyon.creatis.vip.application.client.view.monitor.SimulationTab;
@@ -319,14 +320,8 @@ public class SimulationsContextMenu extends Menu {
                 String tabId =
                     ApplicationConstants.getLaunchTabID(applicationName);
                 Layout.getInstance().removeTab(tabId);
-                Layout.getInstance().addTab(
-                    tabId,
-                    () -> new LaunchTab(
-                        applicationName,
-                        applicationVersion,
-                        applicationClass,
-                        simulationName,
-                        result));
+                RelaunchService.getInstance().relaunch(
+                        applicationName, applicationVersion, applicationClass, simulationName, result, tabId);
             }
         };
         WorkflowService.Util.getInstance().relaunchSimulation(simulationID, callback);
