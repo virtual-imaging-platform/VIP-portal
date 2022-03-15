8c7bd2b65112cdaeae0ee4309f9553854e7998dd
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2022-03-15 15:24:45 +0100

    prepare release 2.2

--- vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/CoreConstants.java
+++ vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/CoreConstants.java
@@ -41,7 +41,7 @@ import java.util.Date;
 public class CoreConstants implements IsSerializable {
 
 
-    public static final String VERSION = "v2.2-dev";
+    public static final String VERSION = "v2.2";
     // Configuration Labels
     public static final String VO_NAME = "vo.name";
     public static final String VO_ROOT = "vo.root";
