1f4506a50a200f7e82bf495794b6d5b86e28d291
Alae ES-SAKI <alaeessaki@gmail.com>
2022-06-22 16:32:58 +0100

    cleaning

--- vip-api/src/main/java/fr/insalyon/creatis/vip/api/KeycloakSpringSecurityConfig.java
+++ vip-api/src/main/java/fr/insalyon/creatis/vip/api/KeycloakSpringSecurityConfig.java
@@ -56,7 +56,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
