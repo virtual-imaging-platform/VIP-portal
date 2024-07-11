9f2c459ebf07fda8000e20855e57a5b9544c9dd3
Sandesh Patil <99490622+sandepat@users.noreply.github.com>
2024-07-11 10:47:55 +0200

    Update CoreConstants.java
    revert back on Version to v2.7-dev

--- vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/CoreConstants.java
+++ vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/CoreConstants.java
@@ -42,7 +42,7 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 public class CoreConstants implements IsSerializable {
 
 
-    public static final String VERSION = "v2.6.1";
+    public static final String VERSION = "v2.7-dev";
     // Configuration Labels
     public static final String VO_NAME = "vo.name";
     public static final String VO_ROOT = "vo.root";
