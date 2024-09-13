c2d51b7187f0a9e3486009542a6cbc9cd0749897
Hippolyte Blot <hippolytebl@gmail.com>
2024-09-13 17:14:05 +0200

    Using REST instead of SOAP in WebServiceEngine

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/simulation/WebServiceEngine.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/simulation/WebServiceEngine.java
@@ -4,16 +4,16 @@
  * This software is a web portal for pipeline execution on distributed systems.
  *
  * This software is governed by the CeCILL-B license under French law and
- * abiding by the rules of distribution of free software.  You can  use, 
+ * abiding by the rules of distribution of free software.  You can  use,
  * modify and/ or redistribute the software under the terms of the CeCILL-B
  * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info". 
+ * "http://www.cecill.info".
  *
  * As a counterpart to the access to the source code and  rights to copy,
  * modify and redistribute granted by the license, users are provided only
  * with a limited warranty  and the software's author,  the holder of the
  * economic rights,  and the successive licensors  have only  limited
- * liability. 
+ * liability.
  *
  * In this respect, the user's attention is drawn to the risks associated
  * with loading,  using,  modifying and/or developing or reproducing the
@@ -22,9 +22,9 @@
  * therefore means  that it is reserved for developers  and  experienced
  * professionals having in-depth computer knowledge. Users are therefore
  * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or 
- * data to be ensured and,  more generally, to use and operate it in the 
- * same conditions as regards security. 
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
  *
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-B license and that you accept its terms.
@@ -34,16 +34,25 @@ package fr.insalyon.creatis.vip.application.server.business.simulation;
 import fr.insalyon.creatis.vip.application.client.view.monitor.SimulationStatus;
 import fr.insalyon.creatis.vip.application.server.business.util.ProxyUtil;
 import fr.insalyon.creatis.vip.core.server.business.Server;
-import java.io.File;
-import java.io.InputStream;
-import java.util.List;
 import localhost.moteur_service_wsdl.Moteur_ServiceLocator;
 import org.apache.axis.EngineConfiguration;
 import org.apache.axis.configuration.FileProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
 
 /**
  * Communicates with a moteur server through a web service.
@@ -56,6 +65,11 @@ import org.springframework.stereotype.Service;
 @Service
 @Scope("prototype")
 public class WebServiceEngine extends WorkflowEngineInstantiator {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     // URI address of the Moteur Web service
     private String addressWS;
@@ -101,77 +115,108 @@ public class WebServiceEngine extends WorkflowEngineInstantiator {
      */
     @Override
     public String launch(String proxyFileName, String userDN)
-            throws
-            java.rmi.RemoteException,
-            javax.xml.rpc.ServiceException {
-        String strProxy = null;
+            throws java.rmi.RemoteException, javax.xml.rpc.ServiceException {
 
         System.setProperty("javax.net.ssl.trustStore", server.getTruststoreFile());
         System.setProperty("javax.net.ssl.trustStorePassword", server.getTruststorePass());
         System.setProperty("javax.net.ssl.trustStoreType", "JKS");
 
-        // String settings = "This is going to contain settings...";
-        // Get Proxy from current User's context
-        if (server.getMyProxyEnabled()) {
-            strProxy = ProxyUtil.readAsString(proxyFileName);
+        String strProxy = ProxyUtil.readAsString(proxyFileName);
+
+        String base64Workflow = Base64.getEncoder().encodeToString(workflow.getBytes(StandardCharsets.UTF_8));
+        String base64Input = Base64.getEncoder().encodeToString(input.getBytes(StandardCharsets.UTF_8));
+        String base64Proxy = Base64.getEncoder().encodeToString(strProxy.getBytes(StandardCharsets.UTF_8));
+        String base64Settings = Base64.getEncoder().encodeToString(settings.getBytes(StandardCharsets.UTF_8));
+
+        String jsonInputString = String.format(
+                "{\"workflow\":\"%s\",\"input\":\"%s\",\"proxy\":\"%s\",\"settings\":\"%s\"}",
+                base64Workflow, base64Input, base64Proxy, base64Settings
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(jsonInputString, headers);
+
+        String url = addressWS + "/submit";
+
+        logger.info("sending rest request to launch workflow");
+        return restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+    }
+
+    @Override
+    public void kill(String workflowID) {
+        System.setProperty("javax.net.ssl.trustStore", server.getTruststoreFile());
+        System.setProperty("javax.net.ssl.trustStorePassword", server.getTruststorePass());
+        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+
+        String url = addressWS + "/kill";
+
+        // Create JSON body
+        String jsonInputString = String.format("{\"workflowID\":\"%s\"}", workflowID);
+
+        // Set headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Create HTTP entity
+        HttpEntity<String> entity = new HttpEntity<>(jsonInputString, headers);
+
+        try {
+            // Send POST request
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
+
+            // Check response status
+            if (response.getStatusCode() != HttpStatus.OK) {
+                throw new RuntimeException("Failed to kill workflow, HTTP response code: " + response.getStatusCodeValue());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while trying to kill the workflow", e);
         }
-
-        //configure WS with the specific wsdd file (fix pb of conflict with grid service)
-        String resourcename = "moteur-client-config.wsdd";
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(resourcename);
-        EngineConfiguration engineConfig = new FileProvider(is);
-        Moteur_ServiceLocator wfS = new Moteur_ServiceLocator(addressWS, engineConfig);
-
-        return wfS.getmoteur_service().workflowSubmit(workflow, input, strProxy, settings);
     }
 
+
     @Override
-    public void kill(String workflowID)
-            throws
-            java.rmi.RemoteException,
-            javax.xml.rpc.ServiceException {
+    public SimulationStatus getStatus(String workflowID) {
 
         System.setProperty("javax.net.ssl.trustStore", server.getTruststoreFile());
         System.setProperty("javax.net.ssl.trustStorePassword", server.getTruststorePass());
         System.setProperty("javax.net.ssl.trustStoreType", "JKS");
 
-        String resourcename = "moteur-client-config.wsdd";
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(resourcename);
-        EngineConfiguration engineConfig = new FileProvider(is);
-        Moteur_ServiceLocator wfS = new Moteur_ServiceLocator(addressWS, engineConfig);
+        String url = addressWS + "/status/" + workflowID;
 
-        wfS.getmoteur_service().killWorkflow(workflowID);
-    }
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
 
-    @Override
-    public SimulationStatus getStatus(String workflowID)
-            throws
-            java.rmi.RemoteException,
-            javax.xml.rpc.ServiceException {
+        // Create HTTP entity with headers
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        System.setProperty("javax.net.ssl.trustStore", server.getTruststoreFile());
-        System.setProperty("javax.net.ssl.trustStorePassword", server.getTruststorePass());
-        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+        try {
+            // Send GET request
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-        String resourcename = "moteur-client-config.wsdd";
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(resourcename);
-        EngineConfiguration engineConfig = new FileProvider(is);
-        Moteur_ServiceLocator wfS = new Moteur_ServiceLocator(addressWS, engineConfig);
+            // Check response status
+            if (response.getStatusCode() != HttpStatus.OK) {
+                throw new RuntimeException("Failed to get status, HTTP response code: " + response.getStatusCodeValue());
+            }
 
-        String workflowStatus = wfS.getmoteur_service().getWorkflowStatus(workflowID);
-        MoteurStatus moteurStatus = MoteurStatus.valueOf(workflowStatus);
-        switch (moteurStatus) {
-
-            case RUNNING:
-                return SimulationStatus.Running;
-            case COMPLETE:
-                return SimulationStatus.Completed;
-            case TERMINATED:
-                return SimulationStatus.Killed;
-            case UNKNOWN:
-                return SimulationStatus.Unknown;
-            default:
-                return SimulationStatus.Unknown;
+            // Process response
+            String workflowStatus = response.getBody();
+            WebServiceEngine.MoteurStatus moteurStatus = WebServiceEngine.MoteurStatus.valueOf(workflowStatus);
+            switch (moteurStatus) {
+                case RUNNING:
+                    return SimulationStatus.Running;
+                case COMPLETE:
+                    return SimulationStatus.Completed;
+                case TERMINATED:
+                    return SimulationStatus.Killed;
+                case UNKNOWN:
+                    return SimulationStatus.Unknown;
+                default:
+                    return SimulationStatus.Unknown;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while getting the workflow status", e);
         }
     }
 
