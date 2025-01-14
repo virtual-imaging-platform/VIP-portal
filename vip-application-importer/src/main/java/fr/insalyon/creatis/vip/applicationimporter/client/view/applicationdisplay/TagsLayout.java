6269c2567b44348847868555ac4e77b1d4f30c39
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-01-14 11:03:41 +0100

    small visual changes, add deletion, bold new tags

--- vip-application-importer/src/main/java/fr/insalyon/creatis/vip/applicationimporter/client/view/applicationdisplay/TagsLayout.java
+++ vip-application-importer/src/main/java/fr/insalyon/creatis/vip/applicationimporter/client/view/applicationdisplay/TagsLayout.java
@@ -3,18 +3,25 @@ package fr.insalyon.creatis.vip.applicationimporter.client.view.applicationdispl
 import com.smartgwt.client.types.DragDataAction;
 import com.smartgwt.client.types.Overflow;
 import com.smartgwt.client.types.TitleOrientation;
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.ImgButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.ComboBoxItem;
 import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
 import com.smartgwt.client.widgets.layout.HLayout;
 import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.smartgwt.client.data.DataSource;
@@ -26,8 +33,10 @@ import fr.insalyon.creatis.vip.application.client.bean.boutiquesTools.BoutiquesA
 import fr.insalyon.creatis.vip.application.client.rpc.ApplicationService;
 import fr.insalyon.creatis.vip.application.client.rpc.ApplicationServiceAsync;
 import fr.insalyon.creatis.vip.applicationimporter.client.view.Constants;
+import fr.insalyon.creatis.vip.core.client.view.CoreConstants;
 import fr.insalyon.creatis.vip.core.client.view.common.AbstractFormLayout;
 import fr.insalyon.creatis.vip.core.client.view.layout.Layout;
+import fr.insalyon.creatis.vip.core.client.view.util.WidgetUtil;
 
 public class TagsLayout extends AbstractFormLayout {
 
@@ -39,8 +48,12 @@ public class TagsLayout extends AbstractFormLayout {
     private final LayoutSpacer spacer;
     private final DataSource dataSource;
 
+    private ListGridRecord rollOverRecord;
+    private HLayout rollOverCanvas;
+
     private DynamicForm entryFormBlock;
     private ComboBoxItem entryForm;
+    private Set<String> newTags;
     
     private BoutiquesApplication boutiques;
 
@@ -55,12 +68,13 @@ public class TagsLayout extends AbstractFormLayout {
         dataSource.setFields(new DataSourceTextField("name").setPrimaryKey(true));
 
         suggestedTags = createBoutiquesList();
-        existingsTags = createList("Existing Tags").setDataSource(dataSource);
+        existingsTags = createList("VIP Existings Tags").setDataSource(dataSource);
         spacer = new LayoutSpacer();
         getDynamicFormInput();
 
         configure();
         loadDatabaseTags();
+        newTags = new HashSet<>();
     }
 
     public List<String> getSelectedTags() {
@@ -81,14 +95,49 @@ public class TagsLayout extends AbstractFormLayout {
 
         hLayout.addMember(vLayoutLeft);
         hLayout.addMember(spacer);
-        hLayout.addMember(suggestedTags);
         hLayout.addMember(existingsTags);
+        hLayout.addMember(suggestedTags);
         addMember(hLayout);
+        setDescription();
+        selectedTags.setShowRollOver(true);
+    }
+
+    private void setDescription() {
+        getMember(0).setPrompt("Please each tags must be an unique string clear for users. For boutiques suggestions you might transform them, for example fsl:true should become fsl as a unique tag.");
     }
 
     private ListGrid createList(String name) {
-        ListGrid list = new ListGrid();
+        ListGrid list = new ListGrid() {
+            @Override
+            protected Canvas getRollOverCanvas(Integer rowNum, Integer colNum) {
+                rollOverRecord = this.getRecord(rowNum);
 
+                if (rollOverCanvas == null) {
+                    rollOverCanvas = new HLayout(3);
+                    rollOverCanvas.setSnapTo("TR");
+                    rollOverCanvas.setWidth(50);
+                    rollOverCanvas.setHeight(22);
+
+                    ImgButton deleteImg = WidgetUtil.getImgButton(CoreConstants.ICON_DELETE, "Delete");
+                    deleteImg.addClickHandler(new ClickHandler() {
+                        @Override
+                        public void onClick(ClickEvent event) {
+                            final String name = rollOverRecord.getAttribute("name");
+                            if (newTags.contains(name)) {
+                                selectedTags.removeData(rollOverRecord);
+                            } else {
+                                selectedTags.removeData(rollOverRecord);
+                                addExistingTag(name);
+                            } 
+                        }
+                    });
+                    rollOverCanvas.addMember(deleteImg);
+                }
+                return rollOverCanvas;
+            }
+        };
+
+        list.setShowRollOver(false);
         list.setWidth(175);
         list.setHeight(250);
         list.setDragDataAction(DragDataAction.MOVE);
@@ -97,18 +146,23 @@ public class TagsLayout extends AbstractFormLayout {
         list.setCanReorderRecords(true);
         list.setFields(new ListGridField("name", name));
 
+        list.setCellFormatter((value, record, rowNum, colNum) -> {
+            if (newTags.contains(value)) {
+                return "<span style='font-weight:bold;'>" + (String) value + "</span>";
+            }
+            return (String) value;
+        });
         return list;
     }
 
     private ListGrid createBoutiquesList() {
-        ListGrid list = createList("Boutiques Tags Suggestion");
+        ListGrid list = createList("Boutiques Descriptor Tags");
 
         list.setWidth(325);
         list.setCanDragRecordsOut(false);
         list.setCanAcceptDroppedRecords(false);
         return list;
     }
-
 
     private void addSelectedTag(String tagName) {
         Record record = new Record();
@@ -135,32 +189,10 @@ public class TagsLayout extends AbstractFormLayout {
         return tags;
     }
 
-    private void loadDatabaseTags() {
-        ApplicationServiceAsync service = ApplicationService.Util.getInstance();
-        List<String> tags = getTags(suggestedTags);
-
-        final AsyncCallback<List<Tag>> callback = new AsyncCallback<>() {
-            @Override
-            public void onFailure(Throwable caught) {
-                Layout.getInstance().setWarningMessage("Unable to load tags:<br />" + caught.getMessage());
-            }
-
-            @Override
-            public void onSuccess(List<Tag> result) {
-                result.forEach((t) -> {
-                    if ( ! tags.contains(t.getName())) {
-                        addExistingTag(t.getName());
-                    }
-                });
-            }
-        };
-        service.getTags(callback);
-    }
-
     private void getDynamicFormInput() {
         entryFormBlock = new DynamicForm();
 
-        entryForm = new ComboBoxItem("autoComplete", "Search/Add custom Tag");
+        entryForm = new ComboBoxItem("autoComplete", "Add/Search custom Tag");
 
         entryForm.setWidth(175);
         entryForm.setTitleOrientation(TitleOrientation.TOP);
@@ -181,6 +213,9 @@ public class TagsLayout extends AbstractFormLayout {
                     if (matchedValue.isPresent()) {
                         record.setAttribute("name", matchedValue.get());
                         dataSource.removeData(record);
+                        existingsTags.fetchData();
+                    } else {
+                        newTags.add(inputValue);
                     }
                     if ( ! getTags(selectedTags).contains(inputValue)) {
                         addSelectedTag(inputValue);
@@ -191,6 +226,28 @@ public class TagsLayout extends AbstractFormLayout {
         });
 
         entryFormBlock.setFields(entryForm);
+    }
+
+    private void loadDatabaseTags() {
+        ApplicationServiceAsync service = ApplicationService.Util.getInstance();
+        List<String> tags = getTags(suggestedTags);
+
+        final AsyncCallback<List<Tag>> callback = new AsyncCallback<>() {
+            @Override
+            public void onFailure(Throwable caught) {
+                Layout.getInstance().setWarningMessage("Unable to load tags:<br />" + caught.getMessage());
+            }
+
+            @Override
+            public void onSuccess(List<Tag> result) {
+                result.forEach((t) -> {
+                    if ( ! tags.contains(t.getName())) {
+                        addExistingTag(t.getName());
+                    }
+                });
+            }
+        };
+        service.getTags(callback);
     }
 
     /**
