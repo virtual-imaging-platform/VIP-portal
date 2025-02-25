88bc8a24eb4563916e844db4ee01a9bdeb446115
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-02-25 11:31:39 +0100

    fix file: paths

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/WorkflowBusiness.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/WorkflowBusiness.java
@@ -299,9 +299,8 @@ public class WorkflowBusiness {
             return parseResult.result;
         }
         // not an external platform parameter, use legacy format
-        String parsedPath = lfcPathsBusiness.parseBaseDir(
-                user, parameterValue);
-        if (!user.isSystemAdministrator()) {
+        String parsedPath = lfcPathsBusiness.parseBaseDir(user, parameterValue);
+        if ( ! user.isSystemAdministrator()) {
             checkFolderACL(user, groups, parsedPath);
         }
         if ( ! parsedPath.equals(parameterValue) // the parameter is a file path
