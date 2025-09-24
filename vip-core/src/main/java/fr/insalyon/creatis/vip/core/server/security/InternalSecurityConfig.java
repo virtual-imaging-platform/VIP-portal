2a9e29845f17844383a3f0578edbfcca5e9b7a64
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-09-24 16:58:16 +0200

    enable cors

--- vip-core/src/main/java/fr/insalyon/creatis/vip/core/server/security/InternalSecurityConfig.java
+++ vip-core/src/main/java/fr/insalyon/creatis/vip/core/server/security/InternalSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -35,7 +36,7 @@ public class InternalSecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(antMatcher(HttpMethod.POST, "/internal/session")).permitAll()
                         .requestMatchers(antMatcher("/internal/**")).authenticated())
-                .cors((cors) -> cors.disable())
+                .cors(Customizer.withDefaults())
                 .csrf((csrf) -> csrf
                         // CSRF token managed by SpringSecurity but MANUALLY setted in the controller
                         // after successfull login
