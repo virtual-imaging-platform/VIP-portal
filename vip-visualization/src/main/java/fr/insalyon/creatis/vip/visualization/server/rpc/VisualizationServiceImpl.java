a57f4838d5719acd9fa0fba015f4ed6ce157a2e8
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-10-21 11:36:52 +0200

    put vip-visualization beans into models/ folder + fix VipException Serialization

--- vip-visualization/src/main/java/fr/insalyon/creatis/vip/visualization/server/rpc/VisualizationServiceImpl.java
+++ vip-visualization/src/main/java/fr/insalyon/creatis/vip/visualization/server/rpc/VisualizationServiceImpl.java
@@ -7,10 +7,10 @@ import fr.insalyon.creatis.vip.core.client.VipException;
 import fr.insalyon.creatis.vip.core.client.view.CoreException;
 import fr.insalyon.creatis.vip.core.models.User;
 import fr.insalyon.creatis.vip.core.server.rpc.AbstractRemoteServiceServlet;
-import fr.insalyon.creatis.vip.visualization.client.bean.Image;
-import fr.insalyon.creatis.vip.visualization.client.bean.VisualizationItem;
 import fr.insalyon.creatis.vip.visualization.client.rpc.VisualizationService;
 import fr.insalyon.creatis.vip.visualization.client.view.VisualizationException;
+import fr.insalyon.creatis.vip.visualization.models.Image;
+import fr.insalyon.creatis.vip.visualization.models.VisualizationItem;
 import fr.insalyon.creatis.vip.visualization.server.business.VisualizationBusiness;
 import jakarta.servlet.ServletException;
 
