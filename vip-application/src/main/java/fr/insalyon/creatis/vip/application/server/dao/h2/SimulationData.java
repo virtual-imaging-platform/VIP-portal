422715e4103035767e3de60fb3d25ad3f758b934
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2025-11-03 17:02:45 +0100

    improve error message and small db repo improvements

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/dao/h2/SimulationData.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/dao/h2/SimulationData.java
@@ -103,7 +103,7 @@ public class SimulationData extends AbstractJobData implements SimulationDAO {
         List<Task> list = new ArrayList<Task>();
         try {
             PreparedStatement ps = connection.prepareStatement(
-                    "SELECT j.id, creation, status, command, file_name, exit_code, node_site, node_name, parameters, "
+                    "SELECT j.id, j.invocation_id, creation, status, command, file_name, exit_code, node_site, node_name, parameters, "
                     + "ms FROM Jobs AS j LEFT JOIN ("
                     + "  SELECT jm.id, minor_status AS ms FROM JobsMinorStatus AS jm RIGHT JOIN ( "
                     + "    SELECT id, MAX(event_date) AS ed FROM JobsMinorStatus GROUP BY id "
@@ -133,7 +133,7 @@ public class SimulationData extends AbstractJobData implements SimulationDAO {
 
         try {
             PreparedStatement ps = connection.prepareStatement(
-                    "SELECT j.id, creation, status, command, file_name, exit_code, node_site, node_name, parameters, "
+                    "SELECT j.id, j.invocation_id, creation, status, command, file_name, exit_code, node_site, node_name, parameters, "
                     + "ms FROM Jobs AS j LEFT JOIN ("
                     + "  SELECT jm.id, minor_status AS ms FROM JobsMinorStatus AS jm RIGHT JOIN ( "
                     + "    SELECT id, MAX(event_date) AS ed FROM JobsMinorStatus GROUP BY id "
@@ -238,7 +238,7 @@ public class SimulationData extends AbstractJobData implements SimulationDAO {
         try {
             Statement stat = connection.createStatement();
             ResultSet rs = stat.executeQuery(
-                    "SELECT j.id, j.invocation_id, creation, status, command, file_name, exit_code, exit_message, " +
+                    "SELECT j.id, j.invocation_id, creation, status, command, file_name, exit_code, " +
                             "node_site, node_name, parameters, ms " +
                             "FROM Jobs AS j " +
                             "LEFT JOIN ( " +
@@ -257,7 +257,8 @@ public class SimulationData extends AbstractJobData implements SimulationDAO {
                     minorStatus = parseMinorStatus(rs.getString("ms"));
                 }
 
-                list.add(new Task( rs.getString("id"), rs.getInt("invocation_id"), rs.getTimestamp("creation"), status,
+                list.add(new Task( rs.getString("id"), rs.getInt("invocation_id"),
+                        rs.getTimestamp("creation"), status,
                         rs.getString("command"), rs.getString("file_name"),
                         rs.getInt("exit_code"), rs.getString("node_site"),
                         rs.getString("node_name"), minorStatus,
