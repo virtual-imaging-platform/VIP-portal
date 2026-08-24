f5d375b18201b27766682691fefddcb4ea5fa468
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2026-08-24 11:25:04 +0200

    target next release: 4.8

--- vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/CoreConstants.java
+++ vip-core/src/main/java/fr/insalyon/creatis/vip/core/client/view/CoreConstants.java
@@ -7,7 +7,7 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 public class CoreConstants implements IsSerializable {
 
 
-    public static final String VERSION = "v4.7";
+    public static final String VERSION = "v4.8-dev";
     // Configuration Labels
     public static final String VO_NAME = "vo.name";
     public static final String VO_ROOT = "vo.root";
